27bfa4e6416f3e4c7bba7893924e92b447c5f4e2
Leonardo Leite <leoinatel@gmail.com>
2022-10-24 15:29:02 -0300

    Iniciando o Projeto.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
